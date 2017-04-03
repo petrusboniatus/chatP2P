9c311067989c1bac06135ca74284cabcfdbdea8f
cout970 <thecout970@gmail.com>
2017-04-03 15:56:54 +0200

    Added RMI to the api Basic client

---
+++ src/main/java/client/Client.java
@@ -0,0 +1,36 @@
+package client;
+
+import api.IClient;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+/**
+ * Created by Carlos Couto Cerdeira on 4/3/17.
+ */
+public class Client extends UnicastRemoteObject implements IClient {
+
+    private FriendManager manager;
+
+    protected Client() throws RemoteException {}
+
+    @Override
+    public boolean stillAlive() throws RemoteException {
+        return true;
+    }
+
+    @Override
+    public void notifyFriendListUpdates() throws RemoteException {
+        if (manager != null) {
+            manager.updateFriends();
+        }
+    }
+
+    public FriendManager getManager() {
+        return manager;
+    }
+
+    public void setManager(FriendManager manager) {
+        this.manager = manager;
+    }
+}
--- src/main/java/client/Main.java
+++ src/main/java/client/Main.java
@@ -1,11 +1,41 @@
 package client;
 
+import java.rmi.RemoteException;
+
 /**
  * Created by Carlos Couto Cerdeira on 4/3/17.
  */
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        Client client = null;
 
+        try {
+            client = new Client();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        ServerConnection handler = new ServerConnection(client, "rmi://localhost:1099/server");
+
+        try {
+            handler.getServer();
+        } catch (Exception e) {
+            System.out.println("Error al connectar con el servidor");
+            System.exit(1);
+        }
+
+        boolean success = handler.tryLogin("", "");
+
+        if (!success){
+            System.out.println("Error de login");
+            System.exit(1);
+        }
+
+        FriendManager manager = new FriendManager(handler);
+        client.setManager(manager);
+
+        System.out.println(manager.getProfiles());
     }
 }
